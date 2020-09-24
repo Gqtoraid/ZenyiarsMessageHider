9601b671c32e3552e725cb23d915384512bcf7df
Chazi <jejojora2@gmail.com>
2020-09-23 22:56:37 -0400

    Update Reference.java

--- src/main/java/io/github/chaziei/messagehider/Reference.java
+++ src/main/java/io/github/chaziei/messagehider/Reference.java
@@ -6,7 +6,7 @@ package io.github.chaziei.messagehider;
 public class Reference 
 {
     public static final String MODID = "hypixelmessagehider";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final String AUTHOR = "Chazi";
     public static final String VERSION_RELEASE_DATE = "September 23rd, 2020";
 }
