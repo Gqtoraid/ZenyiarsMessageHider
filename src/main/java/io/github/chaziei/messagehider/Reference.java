9f0638f6cf99f00656876bd6427bb52edb29f9f0
Chazi <jejojora2@gmail.com>
2020-09-23 22:50:40 -0400

    Update and rename src/main/java/io/github/zenyiar/messagehider/Reference.java to src/main/java/io/github/chaziei/messagehider/Reference.java

---
+++ src/main/java/io/github/chaziei/messagehider/Reference.java
@@ -0,0 +1,11 @@
+package io.github.chaziei.messagehider;
+
+/**
+ * Created by Chazi
+ */
+public class Reference 
+{
+    public static final String MODID = "hypixelmessagehider";
+    public static final String VERSION = "1.1";
+    public static final String VERSION_RELEASE_DATE = "September 23rd, 2020";
+}
